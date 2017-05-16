c6e82ad28b9c9ce4f73c7621f5d61c4fdce4f5a5
flyingulysses <wangyong_coder@qq.com>
2017-05-16 16:56:25 +0800

    sql

---
+++ sql/HBTB_Dict.sql
@@ -0,0 +1,5 @@
+INSERT INTO `ngms`.`dict` (`ID`, `GROUP_CODE`, `CODE`, `NAME`, `TYPE`, `THESORT`, `STATE`, `REMARKS`) VALUES ('170', 'TBHBMONTH.FACTOR', '', '同比环比月报比较类型组', 'G', '35', '1', '同比环比月报比较类型组');
+INSERT INTO `ngms`.`dict` (`ID`, `GROUP_CODE`, `CODE`, `NAME`, `TYPE`, `THESORT`, `STATE`, `REMARKS`) VALUES ('171', 'TBHBMONTH.FACTOR', 'sumfilling', '累计充装量（kg）', 'I', '1', '1', '');
+INSERT INTO `ngms`.`dict` (`ID`, `GROUP_CODE`, `CODE`, `NAME`, `TYPE`, `THESORT`, `STATE`, `REMARKS`) VALUES ('172', 'TBHBMONTH.FACTOR', 'sumflow', '流量计累计流量(m3）', 'I', '2', '1', '');
+INSERT INTO `ngms`.`dict` (`ID`, `GROUP_CODE`, `CODE`, `NAME`, `TYPE`, `THESORT`, `STATE`, `REMARKS`) VALUES ('173', 'TBHBMONTH.FACTOR', 'cardflow', '卡控表总用量（m3）', 'I', '3', '1', '');
+INSERT INTO `ngms`.`dict` (`ID`, `GROUP_CODE`, `CODE`, `NAME`, `TYPE`, `THESORT`, `STATE`, `REMARKS`) VALUES ('174', 'TBHBMONTH.FACTOR', 'gasrate', '气化率 m3/kg', 'I', '4', '1', '');
