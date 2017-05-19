a2a5368eb3b34448cd6760b2fd0914684c17e83d
flyingulysses <wangyong_coder@qq.com>
2017-05-19 11:08:59 +0800

    sql

--- sql/Dict_Device.Classify.sql
+++ sql/Dict_Device.Classify.sql
@@ -1,4 +1,4 @@
-INSERT INTO `ngms`.`dict` (`ID`, `GROUP_CODE`, `CODE`, `NAME`, `TYPE`, `THESORT`, `STATE`, `REMARKS`) VALUES ('175', 'DEVICE.CLASSIFY', NULL, '设备分类租', 'G', '35', '1', '设备分类租');
-INSERT INTO `ngms`.`dict` (`ID`, `GROUP_CODE`, `CODE`, `NAME`, `TYPE`, `THESORT`, `STATE`, `REMARKS`) VALUES ('176', 'DEVICE.CLASSIFY', 'plc', 'PLC', 'I', '1', '1', '');
-INSERT INTO `ngms`.`dict` (`ID`, `GROUP_CODE`, `CODE`, `NAME`, `TYPE`, `THESORT`, `STATE`, `REMARKS`) VALUES ('177', 'DEVICE.CLASSIFY', 'llj', '流量计', 'I', '2', '1', '');
-INSERT INTO `ngms`.`dict` (`ID`, `GROUP_CODE`, `CODE`, `NAME`, `TYPE`, `THESORT`, `STATE`, `REMARKS`) VALUES ('178', 'DEVICE.CLASSIFY', 'card', '卡控表', 'I', '3', '1', '');
+INSERT INTO dict ( `GROUP_CODE`, `CODE`, `NAME`, `TYPE`, `THESORT`, `STATE`, `REMARKS`) VALUES ( 'DEVICE.CLASSIFY', NULL, '设备分类租', 'G', '35', '1', '设备分类租');
+INSERT INTO dict ( `GROUP_CODE`, `CODE`, `NAME`, `TYPE`, `THESORT`, `STATE`, `REMARKS`) VALUES ( 'DEVICE.CLASSIFY', 'plc', 'PLC', 'I', '1', '1', '');
+INSERT INTO dict ( `GROUP_CODE`, `CODE`, `NAME`, `TYPE`, `THESORT`, `STATE`, `REMARKS`) VALUES ( 'DEVICE.CLASSIFY', 'llj', '流量计', 'I', '2', '1', '');
+INSERT INTO dict ( `GROUP_CODE`, `CODE`, `NAME`, `TYPE`, `THESORT`, `STATE`, `REMARKS`) VALUES ( 'DEVICE.CLASSIFY', 'card', '卡控表', 'I', '3', '1', '');
