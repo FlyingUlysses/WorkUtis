2ddfbe12538f664c680e10cbfe33ab3afbcfec86
flyingulysses <wangyong_coder@qq.com>
2017-05-19 11:09:55 +0800

    sql

---
+++ sql/Dict_HBTB.sql
@@ -0,0 +1,5 @@
+INSERT INTO dict ( `GROUP_CODE`, `CODE`, `NAME`, `TYPE`, `THESORT`, `STATE`, `REMARKS`) VALUES ( 'TBHBMONTH.FACTOR', '', '同比环比月报比较类型组', 'G', '35', '1', '同比环比月报比较类型组');
+INSERT INTO dict ( `GROUP_CODE`, `CODE`, `NAME`, `TYPE`, `THESORT`, `STATE`, `REMARKS`) VALUES ( 'TBHBMONTH.FACTOR', 'sumfilling', '累计充装量（kg）', 'I', '1', '1', '');
+INSERT INTO dict ( `GROUP_CODE`, `CODE`, `NAME`, `TYPE`, `THESORT`, `STATE`, `REMARKS`) VALUES ( 'TBHBMONTH.FACTOR', 'sumflow', '流量计累计流量(m3）', 'I', '2', '1', '');
+INSERT INTO dict ( `GROUP_CODE`, `CODE`, `NAME`, `TYPE`, `THESORT`, `STATE`, `REMARKS`) VALUES ( 'TBHBMONTH.FACTOR', 'cardflow', '卡控表总用量（m3）', 'I', '3', '1', '');
+INSERT INTO dict ( `GROUP_CODE`, `CODE`, `NAME`, `TYPE`, `THESORT`, `STATE`, `REMARKS`) VALUES ( 'TBHBMONTH.FACTOR', 'gasrate', '气化率 m3/kg', 'I', '4', '1', '');
