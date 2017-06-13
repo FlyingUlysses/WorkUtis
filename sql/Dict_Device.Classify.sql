3aed503bab27ac4286ceba11f17b9d45bd39fa87
flyingulysses <wangyong_coder@qq.com>
2017-06-13 14:36:49 +0800

    sql

--- sql/Dict_Device.Classify.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO dict ( `GROUP_CODE`, `CODE`, `NAME`, `TYPE`, `THESORT`, `STATE`, `REMARKS`) VALUES ( 'DEVICE.CLASSIFY', NULL, '设备分类租', 'G', '35', '1', '设备分类租');
-INSERT INTO dict ( `GROUP_CODE`, `CODE`, `NAME`, `TYPE`, `THESORT`, `STATE`, `REMARKS`) VALUES ( 'DEVICE.CLASSIFY', 'plc', 'PLC', 'I', '1', '1', '');
-INSERT INTO dict ( `GROUP_CODE`, `CODE`, `NAME`, `TYPE`, `THESORT`, `STATE`, `REMARKS`) VALUES ( 'DEVICE.CLASSIFY', 'llj', '流量计', 'I', '2', '1', '');
-INSERT INTO dict ( `GROUP_CODE`, `CODE`, `NAME`, `TYPE`, `THESORT`, `STATE`, `REMARKS`) VALUES ( 'DEVICE.CLASSIFY', 'card', '卡控表', 'I', '3', '1', '');
